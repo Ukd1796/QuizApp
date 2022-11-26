d18d0a40822f727fa0aa41b23698d2a42fe46e0d
Ukd1796 <ujjwal.kumar@somaiya.edu>
2022-11-26 18:54:49 +0530

    state change commit

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,29 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MyAppState();
+  }
+
+}// this widget gets rebuild
+class _MyAppState extends State<MyApp> { // this stays persistent
+  var _questionIndex = 0;
+  void _answerQuestion() {
+    setState(() {
+         _questionIndex = _questionIndex + 1;
+    }); // this basically tell dart when to change the state and helps in executing build widget again
+    print("Answer Chosen");
+  }
+
+  var questions = [
+    'What is the full form of HTML?',
+    'Wich of the following languages is the oldest language?',
+  ];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -12,7 +34,26 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('My First App'),
         ),
-        body: Text('This is my default text'),
+        body: Column(children: [
+          Text(questions[_questionIndex]),
+          RaisedButton(
+            child: Text('Answer 1'),
+            onPressed:
+                _answerQuestion, // this passes a pointer to function and only to be executed when the user presses the button
+          ),
+          RaisedButton(
+            child: Text('Answer 2'),
+            onPressed: _answerQuestion,
+          ),
+          RaisedButton(
+            child: Text('Answer 3'),
+            onPressed: _answerQuestion,
+          ),
+          RaisedButton(
+            child: Text('Answer 4'),
+            onPressed: _answerQuestion,
+          ),
+        ]),
       ),
     );
   }
